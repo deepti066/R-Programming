2de6ff8ab42117aa31f9bff2f90658d0d475661e
deepti066 <87687853+deepti066@users.noreply.github.com>
2023-12-04 22:02:36 +0530

    Add files via upload

---
+++ 1.Rintroduction.R
@@ -0,0 +1,92 @@
+#To output text - This is a comment
+
+"Hello World!"
+'Hello World!'
+
+#To output numbers
+5
+
+10
+
+5 10 20 #will raise an error
+
+#variable
+
+name1 = "Amit"
+name1
+
+
+name2 = "Neha"
+name = "Deepti"
+name
+
+age = 25
+age
+
+x = 21
+x
+
+Name
+
+name
+
+num = "20"
+
+age = age+5
+age
+
+num = num+5
+
+4*8
+
+text = "Excellent"   
+
+paste("ExcelR is", text) #Concatenate / combine text using python
+paste("Students are", text)
+paste("My name is:",name)
+
+var1 = var2 = var3 = "orange"
+
+#Legal variable names
+
+myvar = "Deepti"
+my_var = "Meenal"
+myVar = "Ganesh"
+MYVAR = "Manisha"
+myvar2 = "Lathika"
+this.year = 2022
+
+#illegal variable names
+
+2myvar = "s"
+my-var = "s"
+my var = "s"
+_my_var = "s"
+my_v@var = "s"
+TRUE = "s"
+
+# Data type in R
+
+x= 10
+class(x) 
+class(name)
+
+#Integer - whole +ve -ve L
+y = 1000L
+class(y)
+
+#Character/String
+x = "R is exciting"
+class (x)
+
+#logical/boolean
+
+x = TRUE # or T, FALSE, F
+class(x)
+x = F
+
+3>5
+5>3 #relational operator
+5==5 #Comparison operator
+1 != 6 # not equal to
+
